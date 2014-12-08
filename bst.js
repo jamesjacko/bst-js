29cb9422ecf5680a0a40e8ea020b98d155da1a9a
JamesJacko <james.ogge@gmail.com>
2014-12-08 23:00:32 +0000

    finished

--- bst.js
+++ bst.js
@@ -1,55 +1,72 @@
+/**
+ * Binary Search Tree implementation, limited to two functions
+ * add(data) and contains(data), tree is intiated with no nodes
+ */
 function BST(){
-	this.root = null;
+  this.root = null;
 
-	this.add = function(data){
-        var newNode = new BST_Node(data);
-        var pointer;
-        if (this.root === null){
-            this.root = newNode;
+  /**
+   * add function adds a new node in a tree in the correct place
+   * will not add if the data already exists.
+   */
+  this.add = function(data){
+    var newNode = new BST_Node(data);
+    var pointer;
+    // if root is null we need to add a new node at that point
+    if (this.root === null){
+      this.root = newNode;
+    } else {
+      pointer = this.root;
+      while(true){
+        if (data < pointer.data){
+          if (pointer.left === null){
+            pointer.left = newNode;
+            break;
+          } else {
+            pointer = pointer.left;
+          }
+        } else if (data > pointer.data){
+          if (pointer.right === null){
+            pointer.right = newNode;
+            break;
+          } else {
+            pointer = pointer.right;
+          }
+        // data already present in tree
         } else {
-            pointer = this.root;
-            while(true){
-                if (data < pointer.data){
-                    if (pointer.left === null){
-                        pointer.left = newNode;
-                        break;
-                    } else {
-                        pointer = pointer.left;
-                    }
-                } else if (data > pointer.data){
-                    if (pointer.right === null){
-                        pointer.right = newNode;
-                        break;
-                    } else {
-                        pointer = pointer.right;
-                    }
-                } else {
-                    break;
-                }
-            }
+            break;
         }
-	};
+      }
+    }
+  };
 
-	this.contains = function(data){
-		var comps = 0;
-		var pointer = this.root;
-		while(pointer){
-			if(data < pointer.data){
-				pointer = pointer.left;
-				comps++;
-			} else if(data > pointer.data){
-				pointer = pointer.right;
-				comps++;
-			} else {
-				return comps;
-			}
-		}
-		return 0;
-	};
+  /**
+   * contains function searches the tree for given data and returns
+   * the length of the search if successful or 0 if unsuccessfull
+   */
+  this.contains = function(data){
+    var comps = 0;
+    var pointer = this.root;
+    while(pointer){
+      if(data < pointer.data){
+        pointer = pointer.left;
+        comps++;
+      } else if(data > pointer.data){
+        pointer = pointer.right;
+        comps++;
+      } else {
+        return comps;
+      }
+    }
+    return 0;
+  };
 }
 
+/**
+ * Implementation of a binary search tree node
+ */
 function BST_Node(nodeData){
-	this.left = null;
-	this.right = null;
-	this.data = nodeData;
+  this.left = null;
+  this.right = null;
+  this.data = nodeData;
 }
--- script.js
+++ script.js
@@ -1,99 +1,154 @@
+/*
+ * Run BSTTest runs the test on the BST
+ */
 function runBSTTest(){
-	var start = new Date().getTime();
-	var comps = [['Size', 'Comparisons']];
-	var max = 0;
-	for(var i = 100; i < 10000; i++){
-		var tree = new BST();
-		var numbers = Array.apply(0, Array(i)).map(function(val, i) { return i; });
+  var start = new Date().getTime();
+  var comps = [['Size', 'Comparisons']];
+  var max = 0;
 
-		numbers.sort(function(){ return Math.random() - 0.5});
-		for(var j = 0; j < numbers.length; j++){
-			tree.add(numbers[j]);
-		}
-		var comparisons = 0;
-		for(var j = 0; j < 1000; j ++){
-			var randomNumber = numbers[Math.floor(Math.random() * numbers.length)];
-			comparisons += tree.contains(randomNumber);
-		}
-		delete tree;
-		comparisons /= 1000;
-		max = comparisons > max? comparisons: max;
-		comps.push([i, comparisons]);
-	}
-	console.log(comps);
-	var end = new Date().getTime();
-	var diff = end - start;
-	drawChart(comps, max);
+  // loop over sizes
+  for(var i = 100; i < 10000; i++){
+    // create new tree
+    var tree = new BST();
+    var numbers = Array.apply(0, Array(i)).map(function(val, i) { return i; });
+    // randomize arrat
+    numbers.sort(function(){ return Math.random() - 0.5});
+
+    // fill tree
+    for(var j = 0; j < numbers.length; j++){
+      tree.add(numbers[j]);
+    }
+    var comparisons = 0;
+
+    // loop over tests
+    for(var j = 0; j < 1000; j ++){
+      // get randmom item from array
+      var randomNumber = numbers[Math.floor(Math.random() * numbers.length)];
+      comparisons += tree.contains(randomNumber);
+    }
+
+    // delete tree (garbage collection isn't greate)
+    delete tree;
+    comparisons /= 1000;
+    // save max value for plotting
+    max = comparisons > max? comparisons: max;
+    comps.push([i, comparisons]);
+  }
+  var end = new Date().getTime();
+  var diff = end - start;
+  drawChart(comps, max);
 }
 
+/*
+ * Run Array Test runs the test on the Binary Search
+ */
 function runArrayTest(){
-	var start = new Date().getTime();
-	var comps = [['Size', 'Comparisons']];
-	var max = 0;
-	for(var i = 100; i < 10000; i++){
-		var numbers = Array.apply(0, Array(i)).map(function(val, i) { return i; });
-		var comparisons = 0;
-		for(var j = 0; j < 1000; j ++){
-			var randomNumber = numbers[Math.floor(Math.random() * numbers.length)];
-			comparisons += binarySearch(numbers, randomNumber);
-		}
-		comparisons /= 1000;
-		max = comparisons > max? comparisons: max;
-		comps.push([i, comparisons]);
-	}
-	var end = new Date().getTime();
-	var diff = end - start;
-	drawChart(comps, max);
+  var start = new Date().getTime();
+  var comps = [['Size', 'Comparisons']];
+  var max = 0;
+  for(var i = 100; i < 10000; i++){
+    var numbers = Array.apply(0, Array(i)).map(function(val, i) { return i; });
+    var comparisons = 0;
+    for(var j = 0; j < 1000; j ++){
+      var randomNumber = numbers[Math.floor(Math.random() * numbers.length)];
+      comparisons += binarySearch(numbers, randomNumber);
+    }
+    comparisons /= 1000;
+    max = comparisons > max? comparisons: max;
+    comps.push([i, comparisons]);
+  }
+  var end = new Date().getTime();
+  var diff = end - start;
+  drawChart(comps, max);
 }
 
+/**
+ * Amalgamation of both test and plots both at the end, including y = log2(x)
+ */
+function runBothTests(){
+  var start = new Date().getTime();
+  var comps = [['Size', 'Binary Search', 'BST', 'Log2(n)']];
+  var max = 0;
+  for(var i = 100; i < 10000; i++){
+    var numbers = Array.apply(0, Array(i)).map(function(val, i) { return i; });
+    var comparisons = 0;
+    for(var j = 0; j < 1000; j ++){
+      var randomNumber = numbers[Math.floor(Math.random() * numbers.length)];
+      comparisons += binarySearch(numbers, randomNumber);
+    }
+    comparisons /= 1000;
+    max = comparisons > max? comparisons: max;
+    var appender = [[0,0,0]];
+    appender[0] = i;
+    appender[1] = comparisons;
+    var tree = new BST();
+    numbers.sort(function(){ return Math.random() - 0.5});
+    for(var j = 0; j < numbers.length; j++){
+      tree.add(numbers[j]);
+    }
+    var comparisons = 0;
+    for(var j = 0; j < 1000; j ++){
+      var randomNumber = numbers[Math.floor(Math.random() * numbers.length)];
+      comparisons += tree.contains(randomNumber);
+    }
+    delete tree;
+    comparisons /= 1000;
+    max = comparisons > max? comparisons: max;
+    appender[2] = comparisons;
+    appender[3] = Math.log2(i);
+    comps.push(appender);
+  }
+  var end = new Date().getTime();
+  var diff = end - start;
+  console.log("Finished computing, plotting graph", comps);
+  drawChart(comps, max);
+}
+
+/**
+ * binarySearch, searches a given array to find a given element
+ */
 function binarySearch(array, find){
-	var start = 0;
-	var end = array.length - 1;
-	var mid = 0;
-	var currentElem;
-	var comps = 0;
+  var start = 0;
+  var end = array.length - 1;
+  var mid = 0;
+  var currentElem;
+  var comps = 0;
 
 
-	while(start <= end){
-		comps++;
-		mid = (start + end) >> 1;
-		currentElem = array[mid];
-		if(find > currentElem)
-			start = mid + 1;
-		else if(find < currentElem)
-			end = mid - 1;
-		else
-			return comps;
-	}
-	return -1;
+  while(start <= end){
+    comps++;
+    mid = (start + end) >> 1;
+    currentElem = array[mid];
+    if(find > currentElem)
+      start = mid + 1;
+    else if(find < currentElem)
+      end = mid - 1;
+    else
+      return comps;
+  }
+  return -1;
 }
 
+/**
+ * Draw google chart of data
+ */
 function drawChart(array, max) {
-    var data = google.visualization.arrayToDataTable(array);
-
+    var data = google.visualization.arrayToDataTable(array, false);
     var options = {
       title: 'Size Vs Comparisons',
       hAxis: {title: 'Size', minValue: 0, maxValue: 10000},
       vAxis: {title: 'Comparisons', minValue: 0, maxValue: max},
-      legend: 'none',
+      legend: 'right',
       pointSize: 1
     };
 
     var chart = new google.visualization.ScatterChart(document.getElementById('chart_div'));
-
+    
     chart.draw(data, options);
 
-    var csvHeader = "data:text/csv;charset=utf-8,";
-    var csvString, csvContent;
-    array.forEach(function(subArray, i){
-	   csvString = subArray.join(",");
-	   csvContent += i < subArray.length ? csvString+ "\n" : csvString;
-	});
-	var encodedUri = encodeURI(csvContent);
-	var link = document.createElement("a");
-	link.setAttribute("href", encodedUri);
-	link.setAttribute("download", "my_data.csv");
-	link.text = "download"
-	//document.body.appendChild(link);
-	//link.click()
+    var csv = google.visualization.dataTableToCsv(data);
+    console.log(csv);
+    
+
+    window.open(chart.getImageURI());
 }
